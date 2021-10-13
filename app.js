10e2857231801e81cc65bffa1936913d509fe6ac
Ittahad Uz Zaman <mdittahad@yahoo.com>
2021-10-13 20:14:07 +0600

    dummy token removed

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ rabbitMq.registerCallback(testCallback, TestCommandAnother);
 
 rabbitMq.prepareChannel(channel => {
   setInterval(() => {
-    token = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MDI2NzUwYTViOTUwYjAwMTc0YzUyMmMiLCJ0ZW5hbnRJZCI6IjFBRjIzODBFLUI2MzQtNDlFOS1CQTFDLTk3NzNFNkMyMEQ0QyIsImVtYWlsIjoiYWthc2hAZ21haWwuY29tIiwicGhvbmUiOiIwMTcyMTA3MDcxNyIsInJvbGVzIjpbImFwcHVzZXIiLCJhZG1pbiJdLCJpYXQiOjE2MTc0NTQyMDgsImV4cCI6MTYxNzQ1NzgwOH0.s2qLWWA4rvzQoei3anK4-uB6PLmnWs3eKXWttjCkNJZEKzqErJcw1x3SnABX_a-cV3526SNOkJrfXyJFu0EEVzZC0J5uN9V0ZT-TZgi2z8dtTE_EtdDdjspcqzma6-cMUlGp-VOhTjsxXERpWT07DWATPXoDQFdB4U6OFNaWQy8aBrBuUPCaj7sPyy2x5B0zAHj1zdfL7lghQ2g7EWmOMHQtMkgbV9Hbco7btq4tWqHTGWj3GO8FJ0MHBVMIapX-uP_hqMXwfH1frp4bjPdcmh6WCZHgFu-gJ3pNG6TpUG9yM2mpx8JEK31t7VAL3ofDfjcQQget6Ei8XQMjVrHn3onNLnoqSldpY6aqbm5fTjCbKAj0NTCCrh3lUXnuI-ZvbxbNQeFHFLneOPWS_9VD1EFZf-xigW7G1YO8QUnOf6cQ3gRfmQggoVuPemH57oDXLZ4hxzQZhsH2JM5V_q2X8DLQZCRZUz1hNJehnHFro4ujCaRm-dwnazArI90aYImu";
+    token = "**TEST_TOKEN**";
     rabbitMq.sendToQueue("TestQueue", channel, token, command);
   }, 200);
 });
